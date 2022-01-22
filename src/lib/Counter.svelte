43a209b525d56202fcb76b2f80a2aa35fd88e0b9
Ayush P Gupta <ayushpguptaapg@gmail.com>
2022-01-22 23:45:05 +0530

    feat: Change UI layout
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,34 +1,20 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+    export let data;
 </script>
 
-<button on:click={increment}>
-  Clicks: {count}
-</button>
+<div
+        class="aspect-square md:aspect-video relative mx-3 max-w-md  bg-zinc-700 overflow-hidden text-white shadow-lg border border-4 border-zinc-600 text-center w-full rounded-lg"
+>
+    <div class="absolute top-0 bottom-0 left-0 right-0 w-full h-full inline-flex justify-center items-center">
+         <pre class="px-3 py-4 font-medium text-base md:text-lg">
+   { data}
+  </pre >
+    </div>
+    <div                  style="font-family: 'Caveat', cursive;"
+                          class="absolute bottom-2 left-0 right-0 font-medium">@nasheeli_aakhein</div>
+
+</div>
 
 <style>
-  button {
-    font-family: inherit;
-    font-size: inherit;
-    padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
-    border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
-    outline: none;
-    width: 200px;
-    font-variant-numeric: tabular-nums;
-    cursor: pointer;
-  }
 
-  button:focus {
-    border: 2px solid #ff3e00;
-  }
-
-  button:active {
-    background-color: rgba(255, 62, 0, 0.2);
-  }
 </style>
